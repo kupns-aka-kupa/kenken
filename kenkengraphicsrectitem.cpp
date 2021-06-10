ea725f1e1ee287c721d438bf75c326359a864c6d
kupns <kupns@mail.ru>
2021-06-10 17:08:38 +0300

    fix puzzle drawing

--- kenkengraphicsrectitem.cpp
+++ kenkengraphicsrectitem.cpp
@@ -4,10 +4,9 @@ void KenkenGraphicsRectItem::paint(QPainter *painter,
                                    const QStyleOptionGraphicsItem *option,
                                    QWidget *widget)
 {
-    QRectF rect = boundingRect();
     painter->setFont(_font);
     painter->setPen(QPen(Qt::black, 5));
-    painter->drawRect(rect);
+    painter->drawPolygon(polygon());
 
     painter->drawText(_cells.front(), Qt::AlignLeft | Qt::AlignTop, _text);
 
@@ -34,13 +33,19 @@ KenkenGraphicsRectItem::KenkenGraphicsRectItem(const Block *block, QGraphicsItem
         return r;
     };
 
-    auto acc = [&](const QPolygon &i, const QRect &p)
-    {
-        return i.united(p);
-    };
-
     std::transform(i.begin(), i.end(), std::back_inserter(_cells), rect);
-    auto polygon = std::accumulate(_cells.begin(), _cells.end(), QPolygon(), acc);
 
-    setPolygon(polygon);
+    QPainterPath path;
+
+    /* TODO use std::for_each with bind instead of foreach
+    {
+        using namespace std::placeholders;
+        std::for_each(_cells.begin(), _cells.end(), std::bind(&QPainterPath::addRect, path, _1));
+    }
+     */
+
+    foreach(auto &cell, _cells)
+        path.addRect(cell);
+
+    setPolygon(path.simplified().toFillPolygon());
 }
